99035f05eca461bc4735ff91f0f54f0bbcb50d27
Sumit7200 <rajsumit7200@gmail.com>
2023-01-11 11:39:01 +0530

    gettttt

---
+++ src/main/java/com/ahom/hrms/Repository/BranchRepository.java
@@ -0,0 +1,7 @@
+package com.ahom.hrms.Repository;
+
+import com.ahom.hrms.entities.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BranchRepository extends JpaRepository<Branch,Integer> {
+}
---
+++ src/main/java/com/ahom/hrms/entities/Branch.java
@@ -0,0 +1,29 @@
+package com.ahom.hrms.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "branchOffice")
+public class Branch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
